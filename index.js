a6b37cbbc5c3e6b11a5a5c0183d49ab625f370af
LukasLapienis <lapienislukasp@gmail.com>
2024-02-06 01:10:09 +0200

    update

--- index.js
+++ index.js
@@ -15,24 +15,3 @@ toggleBtn.onclick = function (params) {
 import { displayEvents } from "./modules/displayEvents.js";
 displayEvents("approvedEvents")
 
-
-import { firebaseConfig } from "../firebase.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import {
-  getDatabase,
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-import {
-  getAuth,
-  onAuthStateChanged,
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase();
-const auth = getAuth(app);
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    const uid = user.uid;
-    console.log(uid)
-  }
-});
--- modules/createEvent.js
+++ modules/createEvent.js
@@ -6,49 +6,59 @@ import {
   set,
   push
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import {
+    getAuth,
+    onAuthStateChanged,
+  } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
+const auth = getAuth(app);
 
-
-const createEventBtn = document.getElementById("createEventButton");
-
-createEventBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    const eventCreateForm = document.getElementById("eventCreateForm")
-    const eventNameInput = document.getElementById("eventNameInput").value
-    const eventLocationInput = document.getElementById("eventLocationInput").value
-    const eventDateInput = document.getElementById("eventDateInput").value
-    const eventTimeInput = document.getElementById("eventTimeInput").value
-    const eventDescriptionInput = document.getElementById("eventDescriptionInput").value
-    const eventImageUrlInput = document.getElementById("eventImageUrlInput").value
-
-    if (
-        !eventNameInput ||
-        !eventLocationInput ||
-        !eventDateInput ||
-        !eventTimeInput ||
-        !eventDescriptionInput||
-        !eventImageUrlInput
-    ) {
-        alert("Please fill in all the fields.");
-        return;
-    }
-
-    set(push(ref(db, "newEvents/")), {
-        name: eventNameInput,
-        location: eventLocationInput,
-        date: eventDateInput,
-        time: eventTimeInput,
-        description: eventDescriptionInput,
-        imageUrl: eventImageUrlInput,
-    })
-        .then(() => {
-            alert("Event added succesfully");
-            eventCreateForm.reset();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    const createEventBtn = document.getElementById("createEventButton");
+    createEventBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+    
+        const eventCreateForm = document.getElementById("eventCreateForm")
+        const eventNameInput = document.getElementById("eventNameInput").value
+        const eventLocationInput = document.getElementById("eventLocationInput").value
+        const eventDateInput = document.getElementById("eventDateInput").value
+        const eventTimeInput = document.getElementById("eventTimeInput").value
+        const eventDescriptionInput = document.getElementById("eventDescriptionInput").value
+        const eventImageUrlInput = document.getElementById("eventImageUrlInput").value
+        const userID = user.uid;
+    
+        if (
+            !eventNameInput ||
+            !eventLocationInput ||
+            !eventDateInput ||
+            !eventTimeInput ||
+            !eventDescriptionInput||
+            !eventImageUrlInput
+        ) {
+            alert("Please fill in all the fields.");
+            return;
+        }
+    
+        set(push(ref(db, "newEvents/")), {
+            name: eventNameInput,
+            location: eventLocationInput,
+            date: eventDateInput,
+            time: eventTimeInput,
+            description: eventDescriptionInput,
+            imageUrl: eventImageUrlInput,
+            creator: userID
         })
-        .catch((err) => {
-            console.log(err);
-        });
+            .then(() => {
+                alert("Event added succesfully");
+                eventCreateForm.reset();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+  }
 });
+
