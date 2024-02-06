175cd3859ac023de8ff68928ca225e8895afdab4
kestulka <kgirdiunas0@gmail.com>
2024-02-06 16:11:04 +0200

    done

--- modules/searchBar.js
+++ modules/searchBar.js
@@ -1,10 +1,6 @@
 import { firebaseConfig } from "../firebase.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  get,
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 import { displayEvents } from "./displayEvents.js";
 
@@ -18,22 +14,5 @@ document
     const queryText = document.getElementById("searchInput").value.trim();
     if (queryText !== "") {
       displayEvents("approvedEvents", queryText);
-      //   const searchResults = [];
-      //   const eventsRef = ref(db, "events");
-
-      //   const eventsSnapshot = await get(eventsRef);
-
-      //   eventsSnapshot.forEach((childSnapshot) => {
-      //     const eventData = childSnapshot.val();
-
-      //     for (const key in eventData) {
-      //       if (eventData[key].toLowerCase().includes(queryText.toLowerCase())) {
-      //         searchResults.push(eventData);
-      //         break;
-      //       }
-      //     }
-      //   });
-
-      //   console.log("Search Results:", searchResults);
     }
   });
