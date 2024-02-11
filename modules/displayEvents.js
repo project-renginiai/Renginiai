b9f6c1f39c76610333071c8175b1506cbf9e28ff
kestulka <kgirdiunas0@gmail.com>
2024-02-11 11:06:18 +0200

    test

--- modules/displayEvents.js
+++ modules/displayEvents.js
@@ -5,12 +5,12 @@ import {
   ref,
   get,
   child,
-  update
+  update,
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 import {
-    getAuth,
-    onAuthStateChanged,
-  } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+  getAuth,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
@@ -52,52 +52,58 @@ export function displayEvents(collectionName, filterByName = "") {
             time.className = "eventTime";
             time.innerText = eventData.time;
 
-
+            // Check if current user is the creator of the event
             onAuthStateChanged(auth, (user) => {
-              if (user) {
-                  const likeButton = document.createElement("button");
-                  likeButton.className = "likeButton";
-                  const heartIcon = document.createElement("i");
-                  heartIcon.className = "fa-solid fa-heart fa-2x";
-                  likeButton.appendChild(heartIcon);
-                  const likeCount = document.createElement("p");
-                  likeCount.className = "likeCount";
-                  likeCount.innerText = `Likes: ${eventData.likes || 0}`;
-                  likeButton.addEventListener("click", async (e) => {
-                      e.preventDefault();    
-                      const eventRef = ref(db, `${collectionName}/${event}`);
-                      const eventLikedByRef = ref(db, `${collectionName}/${event}/likedBy`);
-                      const eventDataSnapshot = await get(eventRef);
-                      if (eventDataSnapshot.exists()) {
-                          const eventData = eventDataSnapshot.val();
-                          const currentLikes = eventData.likes || 0;
-            
-                          if (heartIcon.classList.contains("liked")) {
-                              update(eventRef, {
-                                  likes: currentLikes - 1
-                              });
-                              update(eventLikedByRef, {
-                                  [user.uid]: null
-                              })
-                              
-                          } else {
-                              update(eventRef, {
-                                  likes: currentLikes + 1
-                              });
-                              update(eventLikedByRef, {
-                                [user.uid]: true
-                            })
-                          }
-                          heartIcon.classList.toggle("liked");
-                          likeCount.innerText = `Likes: ${heartIcon.classList.contains("liked") ? currentLikes + 1 : currentLikes -1 }`;
-                      }
-                  });
-                  eventCard.append(likeButton, likeCount)
+              if (user && user.email === eventData.creator) {
+                const likeButton = document.createElement("button");
+                likeButton.className = "likeButton";
+                const heartIcon = document.createElement("i");
+                heartIcon.className = "fa-solid fa-heart fa-2x";
+                likeButton.appendChild(heartIcon);
+                const likeCount = document.createElement("p");
+                likeCount.className = "likeCount";
+                likeCount.innerText = `Likes: ${eventData.likes || 0}`;
+                likeButton.addEventListener("click", async (e) => {
+                  e.preventDefault();
+                  const eventRef = ref(db, `${collectionName}/${event}`);
+                  const eventLikedByRef = ref(
+                    db,
+                    `${collectionName}/${event}/likedBy`,
+                  );
+                  const eventDataSnapshot = await get(eventRef);
+                  if (eventDataSnapshot.exists()) {
+                    const eventData = eventDataSnapshot.val();
+                    const currentLikes = eventData.likes || 0;
+
+                    if (heartIcon.classList.contains("liked")) {
+                      update(eventRef, {
+                        likes: currentLikes - 1,
+                      });
+                      update(eventLikedByRef, {
+                        [user.uid]: null,
+                      });
+                    } else {
+                      update(eventRef, {
+                        likes: currentLikes + 1,
+                      });
+                      update(eventLikedByRef, {
+                        [user.uid]: true,
+                      });
+                    }
+                    heartIcon.classList.toggle("liked");
+                    likeCount.innerText = `Likes: ${
+                      heartIcon.classList.contains("liked")
+                        ? currentLikes + 1
+                        : currentLikes - 1
+                    }`;
+                  }
+                });
+                eventCard.append(likeButton, likeCount);
               } else {
-                const likeButtons = document.querySelectorAll(".likeButton")
-                likeButtons.forEach((el) => el.remove())
-                const likeCounters = document.querySelectorAll(".likeCount")
-                likeCounters.forEach((el) => el.remove())
+                const likeButtons = document.querySelectorAll(".likeButton");
+                likeButtons.forEach((el) => el.remove());
+                const likeCounters = document.querySelectorAll(".likeCount");
+                likeCounters.forEach((el) => el.remove());
               }
             });
             eventsContainer.append(eventCard);
@@ -117,5 +123,111 @@ export function displayEvents(collectionName, filterByName = "") {
     });
 }
 
+export function displayUserEvents(filterByName = "") {
+  const currentUser = auth.currentUser;
 
+  if (!currentUser) {
+    console.log("No user signed in");
+    return;
+  }
 
+  get(child(ref(db), "approvedEvents/"))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const allEvents = snapshot.val();
+        const eventsContainer = document.createElement("div");
+        eventsContainer.id = "eventsContainer";
+
+        for (let eventId in allEvents) {
+          const eventData = allEvents[eventId];
+          // Check if the event name matches the filter
+          if (
+            filterByName === "" ||
+            eventData.name.toLowerCase().includes(filterByName.toLowerCase())
+          ) {
+            // Check if current user is the creator of the event
+            if (currentUser.email === eventData.creator) {
+              const eventCard = document.createElement("div");
+              eventCard.className = "eventCard";
+              const name = document.createElement("p");
+              name.className = "eventName";
+              name.innerText = eventData.name;
+              const location = document.createElement("p");
+              location.className = "eventLocation";
+              location.innerText = eventData.location;
+              const image = document.createElement("img");
+              image.className = "eventImage";
+              image.src = eventData.imageUrl;
+              image.setAttribute("alt", "event image");
+              const description = document.createElement("p");
+              description.className = "eventDescription";
+              description.innerText = eventData.description;
+              const date = document.createElement("p");
+              date.className = "eventDate";
+              date.innerText = eventData.date;
+              const time = document.createElement("p");
+              time.className = "eventTime";
+              time.innerText = eventData.time;
+
+              const likeButton = document.createElement("button");
+              likeButton.className = "likeButton";
+              const heartIcon = document.createElement("i");
+              heartIcon.className = "fa-solid fa-heart fa-2x";
+              likeButton.appendChild(heartIcon);
+              const likeCount = document.createElement("p");
+              likeCount.className = "likeCount";
+              likeCount.innerText = `Likes: ${eventData.likes || 0}`;
+              likeButton.addEventListener("click", async (e) => {
+                e.preventDefault();
+                const eventRef = ref(db, `approvedEvents/${eventId}`);
+                const eventLikedByRef = ref(
+                  db,
+                  `approvedEvents/${eventId}/likedBy`,
+                );
+                const eventDataSnapshot = await get(eventRef);
+                if (eventDataSnapshot.exists()) {
+                  const eventData = eventDataSnapshot.val();
+                  const currentLikes = eventData.likes || 0;
+
+                  if (heartIcon.classList.contains("liked")) {
+                    update(eventRef, {
+                      likes: currentLikes - 1,
+                    });
+                    update(eventLikedByRef, {
+                      [currentUser.uid]: null,
+                    });
+                  } else {
+                    update(eventRef, {
+                      likes: currentLikes + 1,
+                    });
+                    update(eventLikedByRef, {
+                      [currentUser.uid]: true,
+                    });
+                  }
+                  heartIcon.classList.toggle("liked");
+                  likeCount.innerText = `Likes: ${
+                    heartIcon.classList.contains("liked")
+                      ? currentLikes + 1
+                      : currentLikes - 1
+                  }`;
+                }
+              });
+              eventCard.append(likeButton, likeCount);
+
+              eventsContainer.append(eventCard);
+              eventCard.append(name, location, image, description, date, time);
+            }
+          }
+        }
+        const main = document.getElementById("main");
+        // Clear existing content before appending new events
+        main.innerHTML = "";
+        main.append(eventsContainer);
+      } else {
+        console.log("No events found");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
---
+++ modules/myEvents_Favorites.js
@@ -0,0 +1,40 @@
+import { firebaseConfig } from "../firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { displayEvents, displayUserEvents } from "./displayEvents.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const auth = getAuth();
+
+onAuthStateChanged(auth, (user) => {
+  const navbar = document.getElementById("navbaras");
+
+  if (user) {
+    const linksDiv = document.createElement("div");
+    linksDiv.classList.add("eventsFavoritesLinks");
+    linksDiv.style.display = "flex";
+    linksDiv.style.listStyleType = "none";
+    linksDiv.style.padding = "0";
+    linksDiv.style.margin = "0";
+    linksDiv.innerHTML = `
+      <li><a id="myEventsLink" href="#">My Events</a></li>
+      <li style="margin-left: 15px; margin-right: 15px" ><a id="myFavoritesLink" href="#">My Favorites</a></li>
+    `;
+    navbar.appendChild(linksDiv);
+
+    const myEvents = document.getElementById("myEventsLink");
+    myEvents.addEventListener("click", () => {
+      displayUserEvents("approvedEvents", user.email);
+    });
+  } else {
+    const eventsFavoritesDiv = document.querySelector(".eventsFavoritesLinks");
+    if (eventsFavoritesDiv) {
+      eventsFavoritesDiv.remove();
+    }
+  }
+});
